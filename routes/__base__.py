9d84d4c125238661a8cacf8e34c9d562653411ec
Satyam Mishra <satyam.work.only@gmail.com>
2023-06-28 03:40:19 +0530

    added Desktop application download option

--- routes/__base__.py
+++ routes/__base__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file
 
 # Create a blueprint for the home routes
 base_page_bp = Blueprint("landing_page", __name__)
@@ -23,9 +23,26 @@ def become_aggregator():
 
 
 
+
 @base_page_bp.route("/products")
 def products():
     return render_template("/base/download_page.html")
+
+@base_page_bp.route("/products/market_player/raspbian_64_bit")
+def market_player_raspbian_64_bit():
+    path = 'E:/Projects/Python/home_automation/static/files/Market Player'
+    return send_file(path, as_attachment=True)
+
+@base_page_bp.route("/products/aggregator/raspbian_64_bit")
+def aggregator_raspbian_64_bit():
+    path = 'E:/Projects/Python/home_automation/static/files/Aggregator'
+    return send_file(path, as_attachment=True)
+
+@base_page_bp.route("/products/utility/raspbian_64_bit")
+def utility_raspbian_64_bit():
+    path = 'E:/Projects/Python/home_automation/static/files/Utility'
+    return send_file(path, as_attachment=True)
+
 
 
 
