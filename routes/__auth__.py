284b9bf00b8386f497fb2c949700c80c2582b206
Satyam Mishra <satyam.work.only@gmail.com>
2023-07-23 16:02:56 +0530

    fixed few bugs and added name showup for marketplayers

--- routes/__auth__.py
+++ routes/__auth__.py
@@ -18,7 +18,10 @@ from routes.utility.diffi_hellman_EC import (
     get_Shared_Key,
 )
 from routes.utility.fetch_Data import get_Aggregator_Id_From_Username
-from routes.utility.insert_Data import insert_Into_Aggregator_Data_From_Aggregator, insert_Into_Private_Data_From_Aggregator
+from routes.utility.insert_Data import (
+    insert_Into_Aggregator_Data_From_Aggregator,
+    insert_Into_Private_Data_From_Aggregator,
+)
 from .utility.general_methods import (
     get_User_Exception_Details,
     get_User_Type_Route,
@@ -76,6 +79,7 @@ def signup(pass_same=False):
                                     "user-type": user_type,
                                     "private-key": user_private_key_hex,
                                     "username": username,
+                                    "user_address": user_address,
                                 }
                             },
                         }
@@ -89,7 +93,6 @@ def signup(pass_same=False):
                         "public_key": user_public_key_hex,
                         "user_type": user_type,
                         "email_id": email,
-                        "user_address": user_address,
                         "username": username,
                         "utility_public_key": "2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d485977454159484b6f5a497a6a3043415159464b34454541434944596741456d466d7641695041526b554c6b63494e4b6e476369704f57586b7275744539630a772f656f6b7870386a41686d4b79716757646536553372306331417038473058366158536876724755456166313276785076427575356973337868767663515a0a6d576a4753707164716634387362317338384853314364766e663469412b51350a2d2d2d2d2d454e44205055424c4943204b45592d2d2d2d2d0a",
                         # 'mac_address': 'sd',
@@ -158,19 +161,18 @@ def signin():
 
                     supabase_.postgrest.auth(user_access_token)
 
+                    table_name = "private_data"
+                    response = (
+                        supabase_.table(table_name=table_name)
+                        .select("aggregator_id", "utility_public_key")
+                        .eq("user_id", user_id)
+                        .execute()
+                    )
                     if user_metadata["user-type"] not in ["Aggregator", "Utility"]:
                         user_private_key = user_metadata["private-key"]
                         end_time = time.time()
                         time_taken_private_key_exchange = end_time - start_time
                         # for consumer, producer and prosumer
-
-                        table_name = "private_data"
-                        response = (
-                            supabase_.table(table_name=table_name)
-                            .select("aggregator_id", "utility_public_key")
-                            .eq("user_id", user_id)
-                            .execute()
-                        )
 
                         # print(response.data)
                         start_time = time.time()
@@ -223,6 +225,8 @@ def signin():
                         )
                     else:
                         aggregator_private_key = user_metadata["private-key"]
+                        aggregator_id = response.data[0]["aggregator_id"]
+                        utility_public_key = response.data[0]["utility_public_key"]
                         set_User_Session(
                             email=email,
                             user_type=user_metadata["user-type"],
@@ -280,21 +284,21 @@ def aggregator_signup(pass_same=False):
                                     "user-type": user_type,
                                     "private-key": user_private_key_hex,
                                     "username": username,
+                                    "user_address": address,
                                 }
                             },
                         }
                     )
-                    
+
                     row = {
                         "user_id": user.user.id,
                         "public_key": user_public_key_hex,
                         "user_type": user_type,
                         "email_id": email,
-                        "user_address": address,
                         "username": username,
                         "utility_public_key": "2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d485977454159484b6f5a497a6a3043415159464b34454541434944596741456d466d7641695041526b554c6b63494e4b6e476369704f57586b7275744539630a772f656f6b7870386a41686d4b79716757646536553372306331417038473058366158536876724755456166313276785076427575356973337868767663515a0a6d576a4753707164716634387362317338384853314364766e663469412b51350a2d2d2d2d2d454e44205055424c4943204b45592d2d2d2d2d0a",
                     }
-                    
+
                     insert_Into_Private_Data_From_Aggregator(row)
                     insert_Into_Aggregator_Data_From_Aggregator(user.user.id, username)
 
